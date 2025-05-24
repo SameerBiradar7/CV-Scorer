eb429f334b18ed42d31319ffd1f2b17c6aae6438
Mohammad Sameer Biradar <sameerbiradar@rlsbca.edu.in>
2025-05-24 18:43:49 +0530

    Add vercel.json and set debug=False for deployment

--- app.py
+++ app.py
@@ -367,4 +367,4 @@ def score_resume_jd():
                            person_mobile=person_mobile)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0',port=5000)
+    app.run(debug=False, host='0.0.0.0',port=5000)
